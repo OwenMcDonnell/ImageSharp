37b2d8094d81482e47e0d7c98f8d9f8cd44fed2f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-07-13 15:26:34 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -93,7 +93,7 @@ if("$env:APPVEYOR_API_URL" -ne ""){
 }
 
 Write-Host "Building version '${version}'"
-msbuild /t:Restore /p:packageversion=$version
+dotnet restore /p:packageversion=$version
 
 Write-Host "Building projects"
 dotnet build -c Release /p:packageversion=$version
